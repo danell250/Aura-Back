17806f4474c9f71ea5c779dc88b8835ccfb3061c
Danell Oosthuizen <danelloosthuizen3@gmail.com>
2026-01-13 14:59:03 +0200

    Fix route path for testEmail.js

--- src/routes/commentsRoutes.ts
+++ src/routes/commentsRoutes.ts
@@ -7,18 +7,18 @@ const router = Router();
 router.get('/posts/:postId/comments', commentsController.getCommentsByPost);
 
 // GET /api/comments/:id - Get comment by ID
-router.get('/:id', commentsController.getCommentById);
+router.get('/comments/:id', commentsController.getCommentById);
 
 // POST /api/posts/:postId/comments - Create new comment
 router.post('/posts/:postId/comments', commentsController.createComment);
 
 // PUT /api/comments/:id - Update comment
-router.put('/:id', commentsController.updateComment);
+router.put('/comments/:id', commentsController.updateComment);
 
 // DELETE /api/comments/:id - Delete comment
-router.delete('/:id', commentsController.deleteComment);
+router.delete('/comments/:id', commentsController.deleteComment);
 
 // POST /api/comments/:id/react - Add reaction to comment
-router.post('/:id/react', commentsController.reactToComment);
+router.post('/comments/:id/react', commentsController.reactToComment);
 
 export default router;
